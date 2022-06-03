80a05ae05880a7ae5653e61c3dc1055b4400ff66
DeerDean <db.li.apps@gmail.com>
2022-06-03 15:50:18 +0800

    use rust crypto

--- core/src/block.rs
+++ core/src/block.rs
@@ -1,3 +1,5 @@
+use chrono::prelude::*;
+
 pub struct BlockHeader {
     pub time: i64,
     pub tx_hash: String,
@@ -8,4 +10,11 @@ pub struct Block {
     pub header: BlockHeader,
     pub hash: String,
     pub data: String,
+}
+
+
+impl Block {
+    fn set_hash(&mut self) {
+        self.header.time = Utc::now().timestamp();
+    }
 }
--- utils/src/coder.rs
+++ utils/src/coder.rs
@@ -1,5 +1,7 @@
 use bincode;
 use serde::{Deserialize, Serialize};
+use crypto::digest::Digest;
+use crypto::sha3::Sha3;
 
 pub fn my_serialize<T: ?Sized>(value: &T) -> Vec<u8>
 where
@@ -17,6 +19,13 @@ where
     deserialized
 }
 
+pub fn get_hash(bytes: &[u8]) -> String {
+    let mut hasher = Sha3::sha3_256();
+    hasher.input(bytes);
+    hasher.result_str()
+}
+
+// For test
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct Point {
     x: i32,
@@ -28,7 +37,7 @@ mod tests {
     use super::Point;
     use super::{my_deserialize, my_serialize};
     #[test]
-    fn coders_works() {
+    fn coder_works() {
         let point = Point { x: 1, y: 1 };
         let se = my_serialize(&point);
         let de: Point = my_deserialize(&se);
